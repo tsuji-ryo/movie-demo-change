5a37860f3638f2c54742c3b593f6d992f4797fb4
tsuji <>
2018-01-13 22:46:31 +0900

    change s3 hostname

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,9 +13,9 @@ CarrierWave.configure do |config|
   case Rails.env
     when 'production'
       config.fog_directory = 'movie-demo-change'
-      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/本番環境用のバケット名'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/movie-demo-change'
     when 'development'
       config.fog_directory = 'movie-demo-change-development'
-      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/開発環境用のバケット名'
+      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/movie-demo-change-development'
   end
 end
